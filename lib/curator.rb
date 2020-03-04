0194ced7df8b3bb057025cfa9a22305ae1d56717
Chris Stankus <chrisstankus99@gmail.com>
2020-03-04 09:56:48 -0700

    Create photographs by artist test and method.

--- lib/curator.rb
+++ lib/curator.rb
@@ -17,4 +17,15 @@ class Curator
   def find_artist_by_id(id)
     @artists.find {|artist| artist.id == id}
   end
+
+  def photographs_by_artist
+    artists_photographs = {}
+    @artists.each do |artist|
+      photos = @photographs.find_all do |photograph|
+        photograph.artist_id == artist.id
+      end
+      artists_photographs[artist] = photos
+    end
+    artists_photographs
+  end
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,19 +20,38 @@ class CuratorTest < Minitest::Test
          artist_id: "2",
          year: "1941"
     })
+    @photo_3 = Photograph.new({
+         id: "3",
+         name: "Identical Twins, Roselle, New Jersey",
+         artist_id: "3",
+         year: "1967"
+    })
+    @photo_4 = Photograph.new({
+         id: "4",
+         name: "Monolith, The Face of Half Dome",
+         artist_id: "3",
+         year: "1927"
+    })
     @artist_1 = Artist.new({
-        id: "1",
-        name: "Henri Cartier-Bresson",
-        born: "1908",
-        died: "2004",
-        country: "France"
+         id: "1",
+         name: "Henri Cartier-Bresson",
+         born: "1908",
+         died: "2004",
+         country: "France"
     })
     @artist_2 = Artist.new({
-        id: "2",
-        name: "Ansel Adams",
-        born: "1902",
-        died: "1984",
-        country: "United States"
+         id: "2",
+         name: "Ansel Adams",
+         born: "1902",
+         died: "1984",
+         country: "United States"
+    })
+    @artist_3 = Artist.new({
+         id: "3",
+         name: "Diane Arbus",
+         born: "1923",
+         died: "1971",
+         country: "United States"
     })
   end
 
@@ -62,100 +81,23 @@ class CuratorTest < Minitest::Test
     @curator.add_artist(@artist_2)
     assert_equal @artist_1, @curator.find_artist_by_id("1")
   end
-end
 
-# Use TDD to update your Curator class so that is supports the following methods:
-#
-# photographs_by_artist - This method will return a hash artists as keys, and an array of their photographs as values.
-# artists_with_multiple_photographs - This method returns an Array of names of artists who have more than one photograph
-# photographs_taken_by_artists_from(string) - This method takes a String representing a country. It returns an Array of Photographs that were taken by a photographer from that country.
-# The Curator class should now respond to the following interaction pattern:
-#
-# pry(main)> require './lib/photograph'
-# # => true
-#
-# pry(main)> require './lib/artist'
-# # => true
-#
-# pry(main)> require './lib/curator'
-# # => true
-#
-# pry(main)> curator = Curator.new
-# # => #<Curator:0x00007fabc6c2b680...>
-#
-# pry(main)> photo_1 = Photograph.new({
-#      id: "1",
-#      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-#      artist_id: "1",
-#      year: "1954"
-# })
-# # => #<Photograph:0x00007fabc6933180...>
-#
-# pry(main)> photo_2 = Photograph.new({
-#      id: "2",
-#      name: "Moonrise, Hernandez",
-#      artist_id: "2",
-#      year: "1941"
-# })
-# # => #<Photograph:0x00007fabc6c28e58...>
-#
-# pry(main)> photo_3 = Photograph.new({
-#      id: "3",
-#      name: "Identical Twins, Roselle, New Jersey",
-#      artist_id: "3",
-#      year: "1967"
-# })
-# # => #<Photograph:0x00007fabc5bb9ef0...>
-#
-# pry(main)> photo_4 = Photograph.new({
-#      id: "4",
-#      name: "Monolith, The Face of Half Dome",
-#      artist_id: "3",
-#      year: "1927"
-# })
-# # => #<Photograph:0x00007fabc6b931f0...>
-#
-# pry(main)> artist_1 = Artist.new({
-#      id: "1",
-#      name: "Henri Cartier-Bresson",
-#      born: "1908",
-#      died: "2004",
-#      country: "France"
-# })
-# # => #<Artist:0x00007fabc6a52340...>
-#
-# pry(main)> artist_2 = Artist.new({
-#      id: "2",
-#      name: "Ansel Adams",
-#      born: "1902",
-#      died: "1984",
-#      country: "United States"
-# })
-# # => #<Artist:0x00007fabc6c20870...>
-#
-# pry(main)> artist_3 = Artist.new({
-#      id: "3",
-#      name: "Diane Arbus",
-#      born: "1923",
-#      died: "1971",
-#      country: "United States"
-# })
-# # => #<Artist:0x00007fabc5ba0c70...>
-#
-# pry(main)> curator.add_artist(artist_1)
-#
-# pry(main)> curator.add_artist(artist_2)
-#
-# pry(main)> curator.add_artist(artist_3)
-#
-# pry(main)> curator.add_photograph(photo_1)
-#
-# pry(main)> curator.add_photograph(photo_2)
-#
-# pry(main)> curator.add_photograph(photo_3)
-#
-# pry(main)> curator.add_photograph(photo_4)
-#
+  def test_photographs_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    expected = {
+      @artist_1 => [@photo_1],
+      @artist_2 => [@photo_2],
+      @artist_3 => [@photo_3, @photo_4]
+    }
+    assert_equal expected, @curator.photographs_by_artist
+  end
+end
 # pry(main)> curator.photographs_by_artist
 # # => {
 # #        #<Artist:0x00007fabc6a52340...> => [#<Photograph:0x00007fabc6933180...>],
@@ -171,3 +113,7 @@ end
 #
 # pry(main)> curator.photographs_taken_by_artist_from("Argentina")
 # # => []
+
+# photographs_by_artist - This method will return a hash artists as keys, and an array of their photographs as values.
+# artists_with_multiple_photographs - This method returns an Array of names of artists who have more than one photograph
+# photographs_taken_by_artists_from(string) - This method takes a String representing a country. It returns an Array of Photographs that were taken by a photographer from that country.

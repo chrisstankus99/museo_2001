1e0a177c1f9552af4cb6de09cd9f196bee2be1ca
Chris Stankus <chrisstankus99@gmail.com>
2020-03-04 09:17:57 -0700

    Add missing attribute to test.

--- test/artist_test.rb
+++ test/artist_test.rb
@@ -24,27 +24,17 @@ class ArtistTest < Minitest::Test
     assert_equal "2", @artist.id
     assert_equal "Ansel Adams", @artist.name
     assert_equal "1902", @artist.born
+    assert_equal "1984", @artist.died
     assert_equal "United States", @artist.country
+  end
+
+  def test_age_of_death
+    assert_equal
   end
 end
 
 # Use TDD to create a Photograph and an Artist class that respond to the following interaction pattern.
 # For age_at_death, assume an artist was born and died on the same day.
 
-# pry(main)> artist.id
-# #=> "2"
-#
-# pry(main)> artist.name
-# #=> "Ansel Adams"
-#
-# pry(main)> artist.born
-# #=> "1902"
-#
-# pry(main)> artist.died
-# #=> "1984"
-#
-# pry(main)> artist.country
-# #=> "United States"
-#
 # pry(main)> artist.age_at_death
 # #=> 82
